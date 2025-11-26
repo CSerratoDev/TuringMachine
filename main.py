c8af0a30fe3fba2faaaa4a55f5fc3367d2208f2f
Alexis Serrato <alexisserrato@MacBook-Air-de-Alexis.local>
2025-11-26 16:00:55 -0600

    Added Button to create live Turing Machine Model

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import tkinter as tk
+from tkinter import messagebox
 
 root = tk.Tk()
-root.title("Hello World")
-label = tk.Label(root, text= "System by Turing Machine")
+root.geometry("900x800")
+root.title("System by Turing Machine")
+
+def input_text():
+    messagebox.showinfo("Accepting Chain", "creating turing machine with live model")
+
+button = tk.Button(root, text="Create", command=input_text)
+button.pack()
+
+label = tk.Label(root, text= "created by csrratodev")
 label.pack()
 
 root.mainloop()
